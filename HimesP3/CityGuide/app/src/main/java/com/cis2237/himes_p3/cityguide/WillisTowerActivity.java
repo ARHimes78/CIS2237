d679843b3b1e032d1ce58beb5cdb14c7bda1a87b
ARHimes78 <splabbity@Alans-MacBook-Pro.local>
2015-10-11 15:03:05 -0600

    Android p3
    Android p3

---
+++ HimesP3/CityGuide/app/src/main/java/com/cis2237/himes_p3/cityguide/WillisTowerActivity.java
@@ -0,0 +1,13 @@
+package com.cis2237.himes_p3.cityguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WillisTowerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_willis_tower);
+    }
+}
